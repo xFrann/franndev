0d5aeb553afc7b9c5eba0a8d7224c14215beda84
Frangu Madalin <polarfrann@gmail.com>
2022-07-08 22:40:42 +0300

    First commit

---
+++ src/containers/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from "../assets/frann.dev.SVG"  
+import css from "../css/nav.css"
+export default function Nav() {
+  return (
+    <div>
+        <div className='nav-flex'>
+            <div>
+                <img id='nav-logo' src={logo}></img>
+            </div>
+            <div>
+                <ul className='nav-list'>
+                    <li className='nav-item'><a href="#">Home</a></li>
+                    <li className='nav-item'><a href="#">Social</a></li>
+                    <li className='nav-item'><a href="#">Projects</a></li>
+                    <li className='nav-item'><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
