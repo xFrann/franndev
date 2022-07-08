9e5a3adb934dd92acd11910db095f9119f4bb91e
Frangu Madalin <polarfrann@gmail.com>
2022-07-09 00:13:23 +0300

    Home container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Home from "./containers/Home";
 import Nav from "./containers/Nav";
 
 
 function App() {
   return (
-    <Nav></Nav>
+    <>
+      <Nav></Nav>
+      <Home></Home>
+    </>
+
   );
 }
 
