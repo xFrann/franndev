392928d020a8f9e787d3cd4c2c8fe22c6ee7ce3a
Sorin <sorinmartinescu06@gmail.com>
2022-07-09 17:51:45 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import Home from "./containers/Home";
 import Nav from "./containers/Nav";
 import Footer from "./containers/Footer";
+import HomeSorin from "./containers/HomeSorin";
 
 function App() {
   return (
     <>
-      <Nav></Nav>
-      <Home></Home>
-      <Footer></Footer>
+      <Nav/>
+      <HomeSorin/>
+      
+      <Footer/>
     </>
     
   );
 }
 
 export default App;
+//👋 Hello, I'm Madalin
+//Passionate about computer science, still self-teaching about the topic on the dail
---
+++ src/containers/HomeSorin.js
@@ -0,0 +1,20 @@
+
+import HomeSorinStyle from "../css/HomeSorin.css"
+
+export default function HomeSorin() {
+    return(
+        <div className="container">
+                
+                    <h4 className="class-h4">👋 Hello, I'm </h4>
+                    <h3 className="class-h3" id="first-title">Frangu Madalin.</h3>
+                    <h3 className="class-h3">A full stack developer based in Romania</h3>
+                    <p className="class-p">I'm a 21 years old, self-taught, software developer. With every line of code I write I get closer to my dream, to become a software enginner.<br/>
+                    Currently I'm learning and working at the same time, doing freelance work, mainly Static Websites.</p>
+                <button className="button-style class-button">
+                    Ceva important
+                </button>
+        </div>
+    );
+}
+//👋 Hello, I'm Madalin
+//Passionate about computer science, still self-teaching about the topic on the dail
