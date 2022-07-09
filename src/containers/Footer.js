e6e093ad2c3992d1627904de590b850fd0b29664
Sorin <sorinmartinescu06@gmail.com>
2022-07-09 20:44:15 +0200

    linked to social media profiles

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -16,16 +16,16 @@ export default function Footer(){
             <a className="color-grey">©Frann 2022, All rights reserved.</a>
             
             <div className="icons">
-                <a href="#">
-                    <img src={instagram} className="image-icon icons" />
-                </a>
-                <a href="#">
+                <a href="https://github.com/xFrann">
                     <img src={github} className="image-icon icons" />
                 </a>
-                <a href="#">
+                <a href="https://www.instagram.com/frann.dev/">
+                    <img src={instagram} className="image-icon icons" />
+                </a>
+                <a href="https://www.linkedin.com/in/frangumadalin215/">
                     <img src={linkedin} className="image-icon icons" />
                 </a>
-                <a href="#">
+                <a href="mailto:someone@yoursite.com">
                     <img src={mail} className="image-icon icons" />
                 </a>
                          
