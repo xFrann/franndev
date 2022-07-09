ffa70b683b20c1b038d1f8db00bca8c86ad065e4
Sorin <sorinmartinescu06@gmail.com>
2022-07-09 23:34:38 +0200

    small change in home

--- src/containers/HomeSorin.js
+++ src/containers/HomeSorin.js
@@ -5,16 +5,14 @@ export default function HomeSorin() {
     return(
         <div className="container">
                 
-                    <h4 className="class-h4">👋 Hello, I'm </h4>
-                    <h3 className="class-h3" id="first-title">Frangu Madalin.</h3>
-                    <h3 className="class-h3">A full stack developer based in Romania</h3>
+                    
+                    <h3 className="class-h4" id="first-title">👋 Hello, I'm</h3>
+                    <h3 className="class-h3" id="color-blue">Frangu Madalin</h3>
                     <p className="class-p">I'm a 21 years old, self-taught, software developer. With every line of code I write I get closer to my dream, to become a software enginner.<br/>
-                    Currently I'm learning and working at the same time, doing freelance work, mainly Static Websites.</p>
+                    Currently I'm learning and working at the same time, doing freelance work.</p>
                 <button className="button-style class-button" >
                     <a href="#" className="no-text-decoration ">Ceva important</a>
                 </button>
         </div>
     );
 }
-//👋 Hello, I'm Madalin
-//Passionate about computer science, still self-teaching about the topic on the dail
