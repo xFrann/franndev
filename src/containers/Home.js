b8209fc878efdd6c95fc3a5b3fff115cefa5a58c
Frangu Madalin <polarfrann@gmail.com>
2022-07-12 00:49:52 +0300

    small changes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Text from '../components/Text'
 import data from "../assets/pageContent.json"
+import me from "../assets/me.jpeg"
 export default function Home() {
 
 
   return (
     <div>
-
+      
         <Text title={data.home.introduction.title}>{data.home.introduction.content}</Text>
-
+        <Text title={data.home.about.title}>{data.home.about.content}</Text>
+        <img src={me} width="400px" height="700px"></img>
 
     </div>
   )
