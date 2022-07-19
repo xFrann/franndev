283f7e02209d976a24ca49f92b66daf8606491aa
Frangu Madalin <polarfrann@gmail.com>
2022-07-19 15:08:05 +0300

    added html formatting when reading from json file

--- src/components/Text.js
+++ src/components/Text.js
@@ -2,6 +2,7 @@ import React from 'react'
 import css from '../css/text.css'
 
 export default function Text(props) {
+
   return (
     <div className='text-container'>
         <div className='title'>
@@ -9,7 +10,11 @@ export default function Text(props) {
         </div>
 
         <div className='content'>
-            <p>{props.children}</p>
+            {/* 
+                There's no user input that can alter the json file on disk from where the text is loaded
+                So this tag should be safe as long as the children of the Text tag cannot be modified by the user.
+            */}
+            <p dangerouslySetInnerHTML={{__html: props.children}}></p>
         </div>
 
     </div>
