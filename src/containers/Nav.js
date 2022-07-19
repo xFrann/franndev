8649a63638ea1c274c212c31f03892c12fedfb6e
Frangu Madalin <polarfrann@gmail.com>
2022-07-19 16:22:39 +0300

    removed icons from nav

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -21,12 +21,6 @@ export default function Nav() {
                     <li className='nav-item'><Link to="/work">Work</Link></li>
                     <li className='nav-item'><a href="#">Projects</a></li>
                     <li className='nav-item'><a href="#">Contact</a></li>
-                    <div className='nav-item' style={{display: "flex", justifyContent: "space-evenly"}}>
-                        <li className='nav-icon' style={{padding: "0px 3px"}}><a href="#"><img width="16px" height="16px" src={github}></img></a></li>
-                        <li className='nav-icon' style={{padding: "0px 3px"}}><a href="#"><img width="16px" height="16px" src={instagram}></img></a></li>
-                        <li className='nav-icon' style={{padding: "0px 3px"}}><a href="#"><img width="16px" height="16px" src={linkedin}></img></a></li>
-                        <li className='nav-icon' style={{padding: "0px 3px"}}><a href="#"><img width="16px" height="16px" src={mail}></img></a></li>
-                    </div>
                 </ul>
             </div>
         </div>
