b9d6f60e6fde91286a8fa081ad2511b7202e84b3
Frangu Madalin <polarfrann@gmail.com>
2022-07-19 15:29:23 +0300

    moved icons, added links

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -2,6 +2,12 @@ import React from 'react'
 import logo from "../assets/frann.dev.SVG"  
 import css from "../css/nav.css"
 import general from '../css/general.css'
+import github from '../assets/social-media-images/github.svg'
+import instagram from '../assets/social-media-images/instagram.svg'
+import linkedin from '../assets/social-media-images/linkedin.svg'
+import mail from '../assets/social-media-images/mail.svg'
+import { Link } from "react-router-dom"
+
 export default function Nav() {
   return (
     <div>
@@ -11,10 +17,16 @@ export default function Nav() {
             </div>
             <div>
                 <ul className='nav-list'>
-                    <li className='nav-item'><a href="#">Home</a></li>
-                    <li className='nav-item'><a href="#">Work</a></li>
+                    <li className='nav-item'><Link to="/">Home</Link></li>
+                    <li className='nav-item'><Link to="/work">Work</Link></li>
                     <li className='nav-item'><a href="#">Projects</a></li>
                     <li className='nav-item'><a href="#">Contact</a></li>
+                    <div className='nav-item' style={{display: "flex", justifyContent: "space-evenly"}}>
+                        <li className='nav-icon' style={{padding: "0px 3px"}}><a href="#"><img width="16px" height="16px" src={github}></img></a></li>
+                        <li className='nav-icon' style={{padding: "0px 3px"}}><a href="#"><img width="16px" height="16px" src={instagram}></img></a></li>
+                        <li className='nav-icon' style={{padding: "0px 3px"}}><a href="#"><img width="16px" height="16px" src={linkedin}></img></a></li>
+                        <li className='nav-icon' style={{padding: "0px 3px"}}><a href="#"><img width="16px" height="16px" src={mail}></img></a></li>
+                    </div>
                 </ul>
             </div>
         </div>
