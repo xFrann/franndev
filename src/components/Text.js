ecc26baf5b51664b98baa0c0aecfb96dc124a0fa
Frangu Madalin <polarfrann@gmail.com>
2022-07-20 13:17:21 +0300

    edited comment

--- src/components/Text.js
+++ src/components/Text.js
@@ -13,6 +13,7 @@ export default function Text(props) {
             {/* 
                 There's no user input that can alter the json file on disk from where the text is loaded
                 So this tag should be safe as long as the children of the Text tag cannot be modified by the user.
+                Only problem is that Components cannot be rendered, so in case of links the page will be reloaded instead of redirection
             */}
             <p dangerouslySetInnerHTML={{__html: props.children}}></p>
         </div>
