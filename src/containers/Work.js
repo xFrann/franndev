58df15237e3d3dc0a4b7c01bb34533c1713bfb67
Frangu Madalin <polarfrann@gmail.com>
2022-07-20 13:17:41 +0300

    Added work container and necessary styles

--- src/containers/Work.js
+++ src/containers/Work.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import WorkCard from '../components/WorkCard'
+import oracleLogo from '../assets/organizations/oracle.svg'
+import franndevLogo from '../assets/frann.dev.SVG'
+import data from '../assets/pageContent.json'
+
 
 export default function Work() {
   return (
-    <div>Work</div>
+    <div className='work-container'>
+      <div className='work-cards'>
+        <WorkCard organization="Oracle" src={oracleLogo} role="L1 Technical Support" date='July 2021 - Now'>
+          {data.work.oracle.description}
+        </WorkCard>
+        <WorkCard organization='No Organization' role="Freelancer" date='September 2020'>
+          {data.work.freelancer.description}
+        </WorkCard>
+      </div>
+    </div>
   )
 }
