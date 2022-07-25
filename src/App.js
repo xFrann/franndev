4629327383bcb7fb292bf199ebb17e1e63b992c7
Frangu Madalin <polarfrann@gmail.com>
2022-07-25 16:55:42 +0300

    Added route for projects and contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Home from "./containers/Home";
 import Nav from "./containers/Nav";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import Work from "./containers/Work";
+import Projects from "./containers/Projects";
+import Contact from "./containers/Contact";
 function App() {
   return (
     <>
@@ -9,8 +11,10 @@ function App() {
       <BrowserRouter>
       <Nav/>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="work" element={<Work />} />
+          <Route path="/" element={<Home/>}/>
+          <Route path="work" element={<Work/>}/>
+          <Route path="projects" element={<Projects/>}/>
+          <Route path="contact" element={<Contact />}/>
         </Routes>
       </BrowserRouter>
 
