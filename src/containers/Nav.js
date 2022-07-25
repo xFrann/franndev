f983cd8a669245685f001e3477a49eff3ad055fa
Frangu Madalin <polarfrann@gmail.com>
2022-07-25 18:41:39 +0300

    cleaned up imports and added link to logo

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import logo from "../assets/frann.dev.SVG"  
-import css from "../css/nav.css"
-import general from '../css/general.css'
-import github from '../assets/social-media-images/github.svg'
-import instagram from '../assets/social-media-images/instagram.svg'
-import linkedin from '../assets/social-media-images/linkedin.svg'
-import mail from '../assets/social-media-images/mail.svg'
+import "../css/nav.css"
 import { Link } from "react-router-dom"
 
 export default function Nav() {
@@ -13,7 +8,7 @@ export default function Nav() {
     <div>
         <div className='nav-flex'>
             <div>
-                <img id='nav-logo' src={logo}></img>
+                <Link to='/'><img id='nav-logo' src={logo}></img></Link>
             </div>
             <div>
                 <ul className='nav-list'>
